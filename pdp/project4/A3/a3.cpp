0061c64942daf0f8d478d6177ed3546cd373af7f
wolterlw <WolterLentin@gmail.com>
2018-12-16 22:54:08 -0500

    PDP: prefinal CUDA

--- pdp/project4/A3/a3.cpp
+++ pdp/project4/A3/a3.cpp
@@ -10,7 +10,16 @@ int main(int argc, char* argv[]) {
     if (argc != 3) {
         std::cout << "usage: " << argv[0] << " n h" << std::endl;
         return -1;
-    }
+    }std::vector<float> x(n);
+    std::vector<float> y(n, 0.0);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::lognormal_distribution<float> N(0.0, 1.0);
+    std::generate(std::begin(x), std::end(x), std::bind(N, gen));
+
+    // now running your awesome code from a3.hpp
+    
 
     int n = std::atoi(argv[1]);
     float h = std::atof(argv[2]);
