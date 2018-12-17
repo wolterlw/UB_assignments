d749f78be6abb17937f39e22c2979e4a6cbe1e46
wolterlw <wolterlentin@gmail.com>
2018-12-16 22:33:07 -0500

    PDP: working test copy

--- pdp/project4/A3/test.cu
+++ pdp/project4/A3/test.cu
@@ -6,62 +6,37 @@
 #include <random>
 #include <chrono>
 #include <stdio.h>
+#include <fstream>
 
 #define SQRT2PI 2.50662827463100050241
+#define BLOCK_SIZE 512
+#define SHARED_MEM_SIZE 2048
 
 __device__ float gauss_kernel(float x){
     return exp(-x*x / 2) / SQRT2PI;
 }
 
-// __global__ void apply_kernel(int n, float h, float* x, float* y){
-// 	__shared__ float* y_buf;
-// 	__shared__ float* x_buf;
-
-// 	cudaMalloc(&y_buf, n * sizeof(float));
-// 	cudaMalloc(&x_buf, n * sizeof(float));
-
-// 	int gidx = blockIdx.x * blockDim.x + threadIdx.x;
-// 	if (gidx < n){
-// 		float xr = y[gidx];
-// 		y_buf[threadIdx.x] = y[gidx];
-
-// 		for (int i = 0; i < n; i+=blockDim.x){
-// 			j = blockDim.x*(blockIdx.x + j) + threadIdx.x;
-// 			x_buf[threadIdx.x] = x[j];
-
-// 			for (int k = 0; k < blockDim.x; k++){
-// 				y_buf[threadIdx.x] += gauss_kernel((xr - x_buf[k]) / h);
-// 			}
-// 		}
-// 	}
-	
-
-// }
-
 __global__
-void print(int n, float h, float* x, float* y){
-	extern __shared__ float  shared_mem[];
-	float* y_buf = &shared_mem[0];
-	float* x_buf = &shared_mem[blockDim.x];
+void kernel(int n, float h, float* x, float* y){
+	__shared__ float x_buf[SHARED_MEM_SIZE];
+	__shared__ float y_buf[SHARED_MEM_SIZE];
 	float xr;
 
 	int gidx  = blockIdx.x * blockDim.x + threadIdx.x;
 	if (gidx < n){
  		xr = x[gidx];
- 		y_buf[threadIdx.x] = y[gidx];
 
- 		for (int i = 0; i < n; i+=gridDim.x){
- 			int j = blockDim.x*(blockIdx.x + i) + threadIdx.x;
+ 		for (int i = 0; i < gridDim.x; i++){
+ 			int j = blockDim.x*((blockIdx.x + i) % gridDim.x) + threadIdx.x;
  			x_buf[threadIdx.x] = x[j];
 			__syncthreads();
  			for (int k = 0; k < blockDim.x; k++){
  				y_buf[threadIdx.x] += gauss_kernel((xr - x_buf[k]) / h);
  			}
  		}
- 	}
 	y_buf[threadIdx.x] /= (n*h);
-
-	printf("%d: %f\t%f\n", gidx, y_buf[threadIdx.x], xr);
+	y[gidx] = y_buf[threadIdx.x];
+	}
 }
 
 __host__ 
@@ -76,14 +51,15 @@ void gaussian_kde(int n, float h, const std::vector<float>& x, std::vector<float
 	cudaMemcpy(d_x, x.data(), size, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_y, y.data(), size, cudaMemcpyHostToDevice);
 
-	int blockSize = 256;
-	int numBlocks = (n + blockSize - 1) / blockSize;
+	int numBlocks = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
 	std::cout << numBlocks << std::endl ;
-	print<<<numBlocks, blockSize, 2 * blockSize*sizeof(float)>>>(n, h, d_x, d_y);
-	
+	kernel<<<numBlocks, BLOCK_SIZE>>>(n, h, d_x, d_y);
+
 	cudaDeviceSynchronize();
 
-	cudaMemcpy(d_y, y.data(), size, cudaMemcpyDeviceToHost);
+	cudaMemcpy(y.data(), d_y, size, cudaMemcpyDeviceToHost);
+
 	cudaFree(d_x);
 	cudaFree(d_y);
 }
@@ -91,10 +67,10 @@ void gaussian_kde(int n, float h, const std::vector<float>& x, std::vector<float
 int main(int argc, char const *argv[])
 {
 
-	int n = 512;
+	int n = 1000000;
 	float h = 0.1;
 
-	std::vector<float> x(n);
+    std::vector<float> x(n);
     std::vector<float> y(n, 0.0);
 
     std::random_device rd;
@@ -111,5 +87,15 @@ int main(int argc, char const *argv[])
 
     auto elapsed_par = std::chrono::duration<double>(t1 - t0);
     std::cout << "Tp: " << elapsed_par.count() << "s" << std::endl;
+    std::ofstream x_file;
+    std::ofstream y_file;
+
+    x_file.open("x.csv");
+    y_file.open("y.csv");
+
+    for (auto i: x) x_file << i << ",";
+    for (auto i: y) y_file << i << ",";
+    x_file.close();
+    y_file.close();
 	return 0;
 }
